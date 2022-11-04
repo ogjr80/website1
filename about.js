6f863c7f2c1898b880d447f25050cdade11385fc
Gideon Ogongo <ogjr80@gmail.com>
2022-11-04 14:09:40 +0200

    second commit

---
+++ about.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <h2>Welcome to the about page</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, non vero dignissimos exercitationem nam sint molestiae ullam quod sit ab nemo. Necessitatibus provident sequi officiis quae esse perspiciatis accusamus tenetur!
+
+    </p>
+</body>
+</html>
